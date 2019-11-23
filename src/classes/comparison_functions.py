c6c99dc891bb7feb8de2004cb76ce015039ec975
QuimLaz <quimlaz@gmail.com>
2019-11-23 19:17:50 +0100

    value de les descripcions

--- src/classes/comparison_functions.py
+++ src/classes/comparison_functions.py
@@ -1,4 +1,6 @@
 from src.classes.task import Task
+from sklearn.metrics import mean_squared_error
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def importance(parameters, task: Task):
@@ -9,3 +11,12 @@ def importance(parameters, task: Task):
 def comparison_using_importance(parameters, task1: Task, task2: Task):
     return task1 if importance(parameters, task1) > importance(parameters, task2) else task2
 
+
+def compair_two_descriptions(descr1, descr2):
+    vectorizer = TfidfVectorizer(stop_words='english', min_df=3)
+    descr1_values = vectorizer.fit_transform(descr1)
+    descr2_values = vectorizer.fit_transform(descr2)
+    if (sum(descr1_values) > sum(descr2_values)):
+        return sum(descr1_values)
+    else:
+        return sum(descr2_values)
