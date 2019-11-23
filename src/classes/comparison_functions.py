ea7d73bc6f22503d32bed67d03e7753e127d3cd3
QuimLaz <quimlaz@gmail.com>
2019-11-23 19:35:53 +0100

    integration and creation of comparison funciton

--- src/classes/comparison_functions.py
+++ src/classes/comparison_functions.py
@@ -12,11 +12,17 @@ def comparison_using_importance(parameters, task1: Task, task2: Task):
     return task1 if importance(parameters, task1) > importance(parameters, task2) else task2
 
 
-def compair_two_descriptions(descr1, descr2):
+def value_description(description: str) -> float:
     vectorizer = TfidfVectorizer(stop_words='english', min_df=3)
-    descr1_values = vectorizer.fit_transform(descr1)
-    descr2_values = vectorizer.fit_transform(descr2)
-    if sum(descr1_values) > sum(descr2_values):
-        return sum(descr1_values)
-    else:
-        return sum(descr2_values)
+    return sum(vectorizer.fit_transform(description))
+
+
+def importance_with_description(parameters, task: Task):
+    return parameters[0] * task.duration + parameters[1] * task.sla + \
+           parameters[2] * task.category.stress_level + parameters[3] * value_description(
+        task.category.description)
+
+
+def comparison_using_importance_and_description(parameters, task1: Task, task2: Task):
+    return task1 if importance_with_description(parameters, task1) > importance_with_description(parameters,
+                                                                                                 task2) else task2
