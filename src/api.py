964407bb0fc85a2af25db85855d7531e788e7dc1
Ineskay <victordivi98@gmail.com>
2019-11-23 16:49:34 +0100

    finish API functions

--- src/api.py
+++ src/api.py
@@ -1,8 +1,9 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import requests
 import json
 
+from src.classes.evaluation import Evaluations, Evaluation
 from src.classes.scenario import Scenario, ScenarioGuess
 
 
@@ -27,9 +28,21 @@ class API:
         correct = response.json()['value']
         return scenario_guess.task_id == correct, correct
 
+    @classmethod
+    def get_evaluation(cls) -> List[Scenario]:
+        response = requests.get(url=f'{cls.evaluation_url}{cls.team_key}')
+        scenario_list = response.json()
+        return [Scenario.from_json(s) for s in scenario_list]
+
+    @classmethod
+    def post_evaluation(cls, evaluations: Evaluations) -> int:
+        response = requests.post(url=f'{cls.evaluation_url}{cls.team_key}', data=json.dumps(evaluations.to_json()),
+                                 headers={'Content-type': 'application/json', 'Accept': 'application/json'})
+        return response.json()['value']
+
 
 if __name__ == '__main__':
-    sc = API.get_scenario(2)
-    print(len(sc.tasks))
-    scg = ScenarioGuess(sc, sc.tasks[2].id)
-    answer = API.post_scenario(scg)
+    scs = API.get_evaluation()
+    ev = Evaluations([Evaluation(s.scenario_id, s.tasks[2].id) for s in scs])
+    score = API.post_evaluation(ev)
+    print(score)
--- src/classes/evaluation.py
+++ src/classes/evaluation.py
@@ -1,14 +1,25 @@
-from typing import List
+from typing import List, Dict
 
 
 class Evaluation:
 
     def __init__(self, scenario_id: str, task_id: str) -> None:
-        self.scenarioId: str = scenario_id
-        self.taskId: str = task_id
+        self.scenario_id: str = scenario_id
+        self.task_id: str = task_id
+
+    def to_json(self) -> Dict:
+        return {
+            'scenarioId': self.scenario_id,
+            'taskId': self.scenario_id
+        }
 
 
 class Evaluations:
 
     def __init__(self, evaluations: List[Evaluation]) -> None:
         self.evaluations: List[Evaluation] = evaluations
+
+    def to_json(self) -> Dict:
+        return {
+            'evaluations': [e.to_json() for e in self.evaluations]
+        }
